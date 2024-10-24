226d849d058ed370e62ac4014ae343bcdee3d260
Nikolay St. Dimitrov <126271396+nikolay-st-d@users.noreply.github.com>
2024-10-24 11:15:44 +0300

    edit: design

--- index.php
+++ index.php
@@ -16,38 +16,44 @@ $code = htmlspecialchars($code, ENT_QUOTES, 'UTF-8');
 </head>
 
 <body>
-    <?php
+        <?php
 
-    require_once 'captcha_class.php';
-    $captcha = new Captcha($code);
+        require_once 'captcha_class.php';
+        $captcha = new Captcha($code);
 
-    // Optional validation message
-    // Must be placed BEFORE the renderImage method
-    echo '<div class="captcha-validation-message">';
-    echo $captcha->validationMessage();
-    echo '</div>';
+        // Optional validation message
+        // Must be placed BEFORE the renderImage method
+        $validation_message = $captcha->validationMessage();
+        if ($_POST) {
+            echo '<div class="captcha-validation-message">';
+            echo $validation_message;
+            echo '</div>';
+        } else {
+            echo '<div class="captcha-validation-message">';
+            echo 'Captcha Form';
+            echo '</div>';
+        }
 
-    if ($captcha->validateCaptcha($code)) {
-        // Code to execute:
+        if ($captcha->validateCaptcha($code)) {
+            // Code to execute:
+        
+        } else {
+            // Optional code for failed validation:
+        }
 
-    } else {
-        // Optional code for failed validation:
-    }
+        ?>
 
-    ?>
-    
-    <form id="form-with-captcha" method="POST">
-        <input type="text" name="name_1" placeholder="Form field 1">
-        <input type="text" name="name_2" placeholder="Form field 2">
-        <input type="text" name="name_3" placeholder="Form field 3">
-        <div>
-            <img src="<?= $captcha->renderImage(); ?>">
-            <div> = </div>
-            <input required type="input" name="code" placeholder=" ?">
-        </div>
-        <button>SUBMIT</button>
-    </form>
-
+        <form id="form-with-captcha" method="POST">
+            <input type="text" name="name_1" placeholder="Form field 1">
+            <input type="text" name="name_2" placeholder="Form field 2">
+            <input type="text" name="name_3" placeholder="Form field 3">
+            <div>
+                <img src="<?= $captcha->renderImage(); ?>">
+                <div> = </div>
+                <input required type="input" name="code" placeholder=" ?">
+            </div>
+            <button>SUBMIT</button>
+        </form>
 </body>
 
 </html>
